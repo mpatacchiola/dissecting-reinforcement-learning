a290e0e40f4fa92ec2228a4f7f2d5fabb8ad3ecf
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2016-12-30 16:13:52 +0000

    Improving the montecarlo example

--- src/two/gridworld.py
+++ src/two/gridworld.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python
+
+#The MIT License (MIT)
+#Copyright (c) 2016 Massimiliano Patacchiola
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+#CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+#SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#Class for creating a gridworld of arbitrary size and with arbitrary obstacles.
+#Each state of the gridworld should have a reward. The transition matrix is defined
+#as the probability of executing an action given a command to the robot.
+
 import numpy as np
 
 class GridWorld:
@@ -16,12 +30,28 @@ class GridWorld:
         self.state_matrix = np.zeros((tot_row, tot_col))
         self.position = [np.random.randint(tot_row), np.random.randint(tot_col)]
 
-    def setTransitionArray(self, transition_array):
-        if(transition_array.shape != self.transition_array):
-            raise ValueError('The shape of the two matrices must be the same.') 
-        self.transition_array = transition_array        
+    #def setTransitionArray(self, transition_array):
+        #if(transition_array.shape != self.transition_array):
+            #raise ValueError('The shape of the two matrices must be the same.') 
+        #self.transition_array = transition_array        
 
     def setTransitionMatrix(self, transition_matrix):
+        '''Set the reward matrix.
+
+        The transition matrix here is intended as a matrix which has a line
+        for each action and the element of the row are the probabilities to
+        executes each action when a command is given. For example:
+        [[0.55, 0.25, 0.10, 0.10]
+         [0.25, 0.25, 0.25, 0.25]
+         [0.30, 0.20, 0.40, 0.10]
+         [0.10, 0.20, 0.10, 0.60]]
+
+        This matrix defines the transition rules for all the 4 possible actions.
+        The first row corresponds to the probabilities of executing each one of
+        the 4 actions when the policy orders to the robot to go UP. In this case
+        the transition model says that with a probability of 0.55 the robot will
+        go UP, with a probaiblity of 0.25 RIGHT, 0.10 DOWN and 0.10 LEFT.
+        '''
         if(transition_matrix.shape != self.transition_matrix.shape):
             raise ValueError('The shape of the two matrices must be the same.') 
         self.transition_matrix = transition_matrix
@@ -40,21 +70,38 @@ class GridWorld:
         -1 for states which are not walkable.
         +1 for terminal states
          0 for all the walkable states (non terminal)
+        The following matrix represents the 4x3 world
+        used in the series "dissecting reinforcement learning"
+        [[0,  0,  0, +1]
+         [0, -1,  0, +1]
+         [0,  0,  0,  0]]
         '''
         self.state_matrix = state_matrix
 
     def setPosition(self, index_row=None, index_col=None):
+        ''' Set the position of the robot in a specific state.
+
+        '''
         if(index_row is None or index_col is None): self.position = [np.random.randint(tot_row), np.random.randint(tot_col)]
         else: self.position = [index_row, index_col]
 
     def render(self):
+        ''' Print the current world in the terminal.
+
+        O represents the robot position
+        - respresent empty states.
+        # represents obstacles
+        * represents terminal states
+        '''
         graph = ""
         for row in range(self.world_row):
             row_string = ""
             for col in range(self.world_col):
-                if(self.state_matrix[row, col] == 0): row_string += ' - '
-                elif(self.state_matrix[row, col] == -1): row_string += ' # '
-                elif(self.state_matrix[row, col] == +1): row_string += ' * '
+                if(self.position == [row, col]): row_string += u" \u25CB " # u" \u25CC "
+                else:
+                    if(self.state_matrix[row, col] == 0): row_string += ' - '
+                    elif(self.state_matrix[row, col] == -1): row_string += ' # '
+                    elif(self.state_matrix[row, col] == +1): row_string += ' * '
             row_string += '\n'
             graph += row_string 
         print graph            
@@ -62,35 +109,37 @@ class GridWorld:
     def reset(self):
         ''' Set the position of the robot in the bottom left corner.
 
+        It returns the first observation
         '''
         self.position = [self.world_row-1, 0]
+        return self.position
 
     def step(self, action):
         ''' One step in the world.
 
-        observation, reward, done = env.step(action)
+        [observation, reward, done = env.step(action)]
         The robot moves one step in the world based on the action given.
         The action can be 0=UP, 1=RIGHT, 2=DOWN, 3=LEFT
         @return observation the position of the robot after the step
         @return reward the reward associated with the next state
-        @return done it is True if the state is terminal  
+        @return done True if the state is terminal  
         '''
         if(action >= self.action_space_size): 
             raise ValueError('The action is not included in the action space.')
 
         #Based on the current action and the probability derived
         #from the trasition model it chooses a new actio to perform
-        action = np.random.choice(4, 1, p=self.transition_matrix[action])
+        action = np.random.choice(4, 1, p=self.transition_matrix[action,:])
         #action = self.transition_model(action)
 
         #Generating a new position based on the current position and action
-        if(action == 0): new_position = [self.position[0]+1, self.position[1]]
-        elif(action == 1): new_position = [self.position[0], self.position[1]+1]
-        elif(action == 2): new_position = [self.position[0]-1, self.position[1]]
-        elif(action == 3): new_position = [self.position[0], self.position[1]-1]
+        if(action == 0): new_position = [self.position[0]-1, self.position[1]]   #UP
+        elif(action == 1): new_position = [self.position[0], self.position[1]+1] #RIGHT
+        elif(action == 2): new_position = [self.position[0]+1, self.position[1]] #DOWN
+        elif(action == 3): new_position = [self.position[0], self.position[1]-1] #LEFT
 
         #Check if the new position is a valid position
-        print(self.state_matrix)
+        #print(self.state_matrix)
         if (new_position[0]>=0 and new_position[0]<self.world_row):
             if(new_position[1]>=0 and new_position[1]<self.world_col):
                 if(self.state_matrix[new_position[0], new_position[1]] != -1):
@@ -100,12 +149,4 @@ class GridWorld:
         #Done is True if the state is a terminal state
         done = bool(self.state_matrix[self.position[0], self.position[1]])
         return self.position, reward, done
-
-
-
-
-
-
-
-
 
--- src/two/montecarlo.py
+++ src/two/montecarlo.py
@@ -1,29 +1,62 @@
+#!/usr/bin/env python
+
+#The MIT License (MIT)
+#Copyright (c) 2016 Massimiliano Patacchiola
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+#CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+#SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#In this example I will use the class gridworld to generate a 3x4 world
+#in which the cleaning robot will move. In this example the robot follows
+#a policy which is optimal, reaching the terminal state (+1) with high probability.
+
 import numpy as np
 from gridworld import GridWorld
 
 env = GridWorld(3, 4)
+
+#Define the state matrix
 state_matrix = np.zeros((3,4))
 state_matrix[0, 3] = 1
 state_matrix[1, 3] = 1
 state_matrix[1, 1] = -1
 
-reward_matrix = np.zeros((3,4))
-reward_matrix.fill(-0.04)
+#Define the reward matrix
+reward_matrix = np.full((3,4), -0.04)
 reward_matrix[0, 3] = 1
 reward_matrix[1, 3] = -1
 
+#Define the transition matrix
+transition_matrix = np.array([[0.90, 0.05, 0.0, 0.05],
+                              [0.05, 0.90, 0.05, 0.0],
+                              [0.0, 0.05, 0.90, 0.05],
+                              [0.05, 0.0, 0.05, 0.90]])
+
+#Define the policy matrix
+policy_matrix = np.array([[1, 1, 1, 0],
+                          [0, 1, 0, 0],
+                          [0, 0, 0, 3],
+                          [0, 3, 3, 3]])
+
+
 env.setStateMatrix(state_matrix)
 env.setRewardMatrix(reward_matrix)
+env.setTransitionMatrix(transition_matrix)
 
-env.reset()
+#Reset the environment
+observation = env.reset()
+env.render()
 
-while(True):
-    observation, reward, done = env.step(0)
-    print("===========")
+for _ in range(1000):
+    action = policy_matrix[observation[0], observation[1]]
+    observation, reward, done = env.step(action)
+    print("")
+    print("ACTION: " + str(action))
+    print("REWARD: " + str(reward))
+    print("DONE: " + str(done))
     env.render()
-    print(observation)
-    print(reward)
-    print(done)
     if done: break
 
 
